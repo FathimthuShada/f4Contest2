c06fd89d823c7078a4f0f1b7bdf1b21f90dd9074
FathimthuShada <83272049+FathimthuShada@users.noreply.github.com>
2023-04-22 22:49:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import LoginPage from './components/login';
+import ProfilePage from './components/profile';
+import { Route, Routes } from "react-router-dom";
+import './App.css';
+
+
+
+function App() {
+    return (
+  //     <div>
+  // <LoginPage/>
+  // <ProfilePage/>
+  // </div>
+  
+    <Routes>
+          <Route path="" element={<LoginPage/>}/>
+          <Route path="/profile" element={<ProfilePage/>}/>
+    </Routes>
+    
+  
+  )
+}
+
+export default App;
